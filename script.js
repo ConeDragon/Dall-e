e01ad03192571fc0afac07fd8e7d4774852d2670
Lobster-Juice <120068827+Lobster-Juice@users.noreply.github.com>
2023-03-24 14:26:57 -0400

    add stuff that does nothing
    well it actually opens the image

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ if (Boolean(localStorage.api)) {
   document.getElementById("api").value = localStorage.api;
 }
 
+
+
 function generate() {
   let api = document.getElementById("api").value;
   let query = document.getElementById("query").value;
+  let url = 'https://api.openai.com/v1/images/generations';
+  let data = {"size": "256x256"};
+  data.prompt = query;
+  let token = 'Bearer ' + api;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    },
+    body: JSON.stringify(data)
+  }).then(response => response.json()).then(data => open(data.data[0].url)).catch(error => alert(error));
 }
