13eaefbaad847b88b429fe72eafc6daada3b4906
Lobster-Juice <120068827+Lobster-Juice@users.noreply.github.com>
2023-03-24 14:19:56 -0400

    variables

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ if (Boolean(localStorage.api)) {
   document.getElementById("api").value = localStorage.api;
 }
 
-function generate() {}
+function generate() {
+  let api = document.getElementById("api").value;
+  let query = document.getElementById("query").value;
+}
